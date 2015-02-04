f19361a39f0464adb32a636be73d4c4706a39395
Mishal Islam <mishal.islam01@gmail.com>
2015-02-04 15:59:52 +0000

    refactored check rows method

--- lib/grid.rb
+++ lib/grid.rb
@@ -16,20 +16,40 @@ class Grid
   end
 
   def checkRows(squarePosition, cellPosition, cellContent)
-    squareRoot = Math.sqrt(@size)
-    @squares.each_slice(squareRoot) do |rowOfSquares|
-      if rowOfSquares.include?(@squares[squarePosition])
-        rowOfSquares.each do |squares|
-          squares.cells.each_slice(squareRoot) do |rowOfCells| 
-            if rowOfCells.include?(squares.cells[cellPosition])
-              rowOfCells.each do |cell|
-                raise "Number is in row already" if cell.content === cellContent
-              end
-            end
-          end
-        end
-      end
+    @squareRoot = Math.sqrt(@size)
+    @squares.each_slice(@squareRoot) do |rowOfSquares|
+      checkRowOfSquares(rowOfSquares, squarePosition, cellPosition, cellContent)
     end
+  end
+
+  def checkRowOfSquares(rowOfSquares, squarePosition, cellPosition, cellContent)
+    if rowOfSquares.include?(@squares[squarePosition])
+      iterateThroughSquares(rowOfSquares, cellPosition, cellContent)
+    end    
+  end
+
+  def iterateThroughSquares(rowOfSquares, cellPosition, cellContent)
+    rowOfSquares.each do |squares|
+      sliceSquares(squares, cellPosition, cellContent)
+    end    
+  end
+
+  def sliceSquares(squares, cellPosition, cellContent)
+    squares.cells.each_slice(@squareRoot) do |rowOfCells| 
+      checkEachRow(rowOfCells, squares, cellPosition, cellContent)
+    end    
+  end
+
+  def checkEachRow(rowOfCells, squares, cellPosition, cellContent)
+    if rowOfCells.include?(squares.cells[cellPosition])
+      checkIfNumberIsInRow(rowOfCells, cellContent)
+    end    
+  end
+
+  def checkIfNumberIsInRow(rowOfCells, cellContent)
+    rowOfCells.each do |cell|
+      raise "Number is in row already" if cell.content === cellContent
+    end    
   end
 
 end
